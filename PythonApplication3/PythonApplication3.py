2be36feaade69c9cf59e6e072b25df7d7edd4f50
Альтмарк Александр Моисеевич <alexaltmark@gmail.com>
2019-01-18 19:37:36 +0300

    111

--- PythonApplication3/PythonApplication3.py
+++ PythonApplication3/PythonApplication3.py
@@ -1,6 +1,6 @@
 from visual import *
 
 
-scene = display(title="Gas", width=win, height=win, x=0, y=0,center=(0,0,0))
 
-s1=sphere(pos=(0,0,0),radius=1, color=color.yellow,  make_trail=True, retain=100)
+
+s1=sphere(pos=(0,0,0),radius=1, color=color.yellow,  make_trail=True, retain=10)
